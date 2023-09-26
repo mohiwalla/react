02373dbdc796fbd8a266912624b5e454ac0afae7
mohiwalla <121000462+mohiwalla@users.noreply.github.com>
2023-09-26 07:08:56 +0530

    fix/ `<ul>` tag was closed before it should be

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -12,7 +12,7 @@ export default function ListGroup({ items, heading }: listArgs) {
         <>
             <h1 className="text-danger">{heading}</h1>
             {items.length === 0 && <h3>No results found ¯\_(ツ)_/¯</h3>}
-            <ul className="list-group w-25"></ul>
+            <ul className="list-group w-25">
                 {items.map((item: string, index: number) => {
                     return (
                         <li
